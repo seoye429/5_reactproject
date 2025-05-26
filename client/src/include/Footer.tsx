a98d1bff04aae1b31909c96a6c25fd3ecf2e3014
seoye429 <seoyei77@gmail.com>
2025-05-26 12:11:25 +0900

    Add files via upload

---
+++ client/src/include/Footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Component} from "react";
+export{};
+
+
+class Footer extends Component {
+    render(){
+ return(
+<>
+<footer>
+    <div className="container-fluid ">
+        <div className="row">
+            <div className="col-md-12 py-3 text-center">
+copyright &copy 2025 all rights reserved.                
+            </div>
+        </div>
+    </div>
+</footer>        
+</>
+    );
+    }
+   
+}
+export default Footer;
+
+
